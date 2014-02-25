476653d8613e8efda62f053e219674b315c57a80
Kevin Cantu <me@kevincantu.org>
2014-02-25 05:16:07 -0800

    now updated for Rust 4243cad56bbd2 :D

--- src/rustx/main.rs
+++ src/rustx/main.rs
@@ -1,19 +1,21 @@
-extern crate extra;
+extern crate test;
 
 use std::io;
 
 #[main]
+#[cfg(not(test))]
 fn say_hi () {
    io::println("Hello!!");
 }
 
 #[test]
 fn addition_works () {
+   io::println("Hello!!");
    assert! (2 + 2 == 4);
 }
 
 #[bench]
-fn addition_benchmarked (b: &mut extra::test::BenchHarness) {
+fn addition_benchmarked (b: &mut test::BenchHarness) {
    let mut sum = 0;
-   b.iter(|| sum += 1)
+   b.iter(|| sum += 1);
 }
